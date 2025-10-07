ac1f4d17ae9f42a67882e4e0b7a23917605eb83f
stone <2328257595@qq.com>
2025-10-07 15:29:31 +0800

    building ...

--- src/modules/database/database.rs
+++ src/modules/database/database.rs
@@ -1,5 +1,4 @@
 use super::error::Error;
-use fslock::LockFile;
 use std::{
     fs::{self, File},
     io::Write,
@@ -17,34 +16,49 @@ pub fn create(name: impl AsRef<Path>) -> Result<(), Error> {
     let mut create_f = File::create_new(path.join("create"))?;
     File::create_new(path.join("delete"))?;
     File::create_new(path.join("config"))?;
-    let mut index_f = File::create_new(path.join("00000000.INDEX"))?;
-    File::create_new(path.join("00000000.DATA"))?;
+    let mut data_f = File::create_new(path.join("00000000.DATA"))?;
     create_f.write_all("00000001".as_bytes())?;
-    index_f.write_all(&[0u8])?;
+    data_f.write_all(&[0u8])?;
     Ok(())
 }
 
 pub fn delete(name: impl AsRef<Path>) -> Result<(), Error> {
     let path = name.as_ref();
-    if !path.is_dir() {
+    if !path.exists() {
         return Err(Error::DatabaseNotFound);
     }
-    if let Ok(mut lock) = LockFile::open(&path.join("lock")) {
-        if !lock.try_lock()? {
-            return Err(Error::DatabaseInUse);
+    if !path.join("lock").is_file() {
+        return Ok(fs::remove_dir_all(path)?);
+    };
+    if let Ok(lock) = File::open(path.join("lock")) {
+        match lock.try_lock() {
+            Ok(()) => (),
+            Err(fs::TryLockError::WouldBlock) => return Err(Error::DatabaseInUse),
+            Err(fs::TryLockError::Error(e)) => return Err(Error::IoError(e)),
         }
     }
     Ok(fs::remove_dir_all(path)?)
 }
 
 pub fn exists(name: impl AsRef<Path>) -> bool {
-    name.as_ref().is_dir()
+    let path = name.as_ref();
+    path.is_dir()
+        && path.join("File").is_dir()
+        && path.join("lock").is_file()
+        && path.join("create").is_file()
+        && path.join("delete").is_file()
+        && path.join("config").is_file()
+        && path.join("00000000.DATA").is_file()
 }
 
 pub fn init() -> Result<(), Error> {
     if exists("system") {
         return Ok(());
     }
+    if Path::new("system").exists() {
+        delete("syatem")?
+    }
     create("system")?;
+    todo!();
     Ok(())
 }
